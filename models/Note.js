46a0f91767bc04b392ed236b30a7f18379234982
Derek <baldka1109bh@gmail.com>
2023-01-17 07:26:07 +0800

    Create note model

---
+++ models/Note.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const AutoIncrement = require("mongoose-sequence")(mongoose);
+
+const noteSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    text: {
+      type: String,
+      required: true,
+    },
+    completed: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+// Make ticket ids start from 1000 and increment in digits vs encrypted long text
+noteSchema.plugin(AutoIncrement, {
+    inc_field: "ticket",
+    id: "ticketNums",
+    start_seq: 1000,
+})
+
+module.exports = mongoose.model("Note", noteSchema);
