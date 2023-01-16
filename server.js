ee5ea078518b4963f50abaf7d1a708bee0901053
Derek <baldka1109bh@gmail.com>
2023-01-17 06:35:26 +0800

    Log every event

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const { logger } = require("./logs/logger");
 const PORT = process.env.PORT || 3500;
 
-app.use("/", express.static(path.join(__dirname, "/public")));
+// Log every req, res event
+app.use(logger);
+
+app.use("/", express.static(path.join(__dirname, "public")));
 app.use("/", require("./routes/root"));
 
 // Handle non-existing paths
