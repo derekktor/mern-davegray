afa0eb264549a97333ca2d17f94df4e8404abcb9
Derek <baldka1109bh@gmail.com>
2023-01-17 06:45:45 +0800

    Handle errors with custom error handler

--- server.js
+++ server.js
@@ -2,10 +2,18 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const { logger } = require("./middleware/logger");
+const errorHandler = require("./middleware/errorHandler");
+const cookieParser = require("cookie-parser");
 const PORT = process.env.PORT || 3500;
 
 // Log every req, res event
 app.use(logger);
+
+// Process json files
+app.use(express.json());
+
+// Parse cookies
+app.use(cookieParser);
 
 app.use("/", express.static(path.join(__dirname, "public")));
 app.use("/", require("./routes/root"));
@@ -21,5 +29,7 @@ app.all("*", (req, res) => {
         res.type("txt").send("Page not found!!!");
     }
 })
+
+app.use(errorHandler)
 
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
